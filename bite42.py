8251d0a9302a230755cb7c04e05dbb0bfe99731d
neternefer <alkarinqe@yahoo.com>
2019-07-17 15:18:33 +0200

    Added bite 42

---
+++ bite42.py
@@ -0,0 +1,90 @@
+MAX_GUESSES = 5
+START, END = 1, 20
+import random
+
+def get_random_number():
+    """Get a random number between START and END, returns int"""
+    return random.randint(START, END)
+
+
+class Game:
+    """Number guess class, make it callable to initiate game"""
+
+    def __init__(self):
+        """Init _guesses, _answer, _win to set(), get_random_number(), False"""
+        self._guesses = set()
+        self._answer = get_random_number()
+        self._win = False
+
+    def guess(self):
+        """Ask user for input, convert to int, raise ValueError outputting
+           the following errors when applicable:
+           'Please enter a number'
+           'Should be a number'
+           'Number not in range'
+           'Already guessed'
+           If all good, return the int"""
+        
+        
+        guess = input(f'Guess a number between {START} and {END}: ')
+        if not guess:
+            raise ValueError('Please enter a number')
+
+        try:
+            guess = int(guess)
+        except ValueError:
+            raise ValueError('Should be a number')
+
+        if guess not in range(START, END+1):
+            raise ValueError('Number not in range')
+
+        if guess in self._guesses:
+            raise ValueError('Already guessed')
+
+        self._guesses.add(guess)
+        return guess
+
+        
+    def _validate_guess(self, guess):
+        """Verify if guess is correct, print the following when applicable:
+           {guess} is correct!
+           {guess} is too low
+           {guess} is too high
+           Return a boolean"""
+        if guess == self._answer:
+            print(f'{guess} is correct!')
+            return True
+        if guess > self._answer:
+            print(f'{guess} is too high')
+            return False
+        else:
+            print(f'{guess} is too low')
+            return False
+
+    def __call__(self):
+        """Entry point / game loop, use a loop break/continue,
+           see the tests for the exact win/lose messaging"""
+        while len(self._guesses) < MAX_GUESSES:
+                try:
+                    gg = self.guess()
+                except Exception as e:
+                    print(e)
+                    continue
+                result = self._validate_guess(gg)
+                if result:
+                    str_guess = len(self._guesses) == 1 and "guess" or "guesses"
+                    self._win = True
+                    print(f'It took you {len(self._guesses)} {str_guess}')
+                    break
+                
+
+        print(f'Guessed 5 times, answer was {self._answer}')
+        
+
+if __name__ == '__main__':
+    game = Game()
+    game()
+
+
+
+ 
